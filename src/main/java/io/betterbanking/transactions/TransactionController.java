a6f91789b6cfe07946facbcefcc664b2289a74a3
Zlatomir Iliev <zlatomir.iliev@telenor.se>
2021-05-05 21:43:40 +0200

    Improve docs

--- src/main/java/io/betterbanking/transactions/TransactionController.java
+++ src/main/java/io/betterbanking/transactions/TransactionController.java
@@ -3,11 +3,13 @@ package io.betterbanking.transactions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController("/api")
+@RequestMapping(path = "/api", produces = "application/json")
 public class TransactionController {
     @Autowired
     private TransactionService transactionService;
